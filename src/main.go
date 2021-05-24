42f168f05e16866cc1c5f2c983dfba995db0a4c1
Harman <hrrsandhu6@gmail.com>
2021-05-24 11:58:51 -0700

    main.go to test leveldb and dockerenv

---
+++ src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type App struct {
+	db *leveldb.DB
+}
+
+func (a *App) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(200)
+	res.Write([]byte("hello, world"))
+	return
+}
+
+func main() {
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
+	rand.Seed(time.Now().Unix())
+	port := flag.Int("port", 8000, "Port for the master server.")
+	dbPath := flag.String("db", "", "Path to leveldb")
+	flag.Parse()
+
+	cmd := flag.Arg(0)
+	if cmd != "serve" && cmd != "rebuild" && cmd != "rebalance" {
+		fmt.Println("Usage: ./steres <serve, rebuild, rebalance>")
+		flag.PrintDefaults()
+		return
+	}
+
+	app := App{
+		db: dbPath,
+	}
+	if cmd == "serve" {
+		http.ListenAndServe(fmt.Sprintf(":%d", *port), &app)
+	} else {
+		fmt.Println("Other options not working at the moment.")
+	}
+
+}
