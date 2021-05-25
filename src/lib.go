4f58a4e0b5566f727626d10b818e7e427ca290d3
Harman <hrrsandhu6@gmail.com>
2021-05-25 10:52:46 -0700

    hash functions, TODO - key2path or key2request

--- src/lib.go
+++ src/lib.go
@@ -1,23 +1,65 @@
 package main
 
-import "strings"
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"sort"
+)
 
-func NSplit(seq []byte, size int) []string {
-	sep := " "
-	s := string(seq)
-	tks := strings.Split(s, sep)
-	tokenizedSequence := []string{}
-	for i := 0; i < len(tks); i++ {
-		tmp := ""
-		for c := 0; c < size; c++ {
-			if i+c < len(tks) {
-				tmp += tks[i+c] + " "
-			} else {
-				break
-			}
+// hash functions
+// we wanna hash Nodes not the node
+// func keyToPath(key []byte)
 
-		}
-		tokenizedSequence = append(tokenizedSequence, string(tmp))
+func key2path(key []byte) string {
+	//
+
+	//create request
+}
+
+type sortvol struct {
+	score  []byte
+	volume string
+}
+type byScore []sortvol
+
+func (s byScore) Len() int      { return len(s) }
+func (s byScore) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s byScore) Less(i, j int) bool {
+	return bytes.Compare(s[i].score, s[j].score) == 1
+}
+
+func key2volume(key []byte, volumes []string, count int, svcount int) []string {
+	// this is an intelligent way to pick the volume server for a file
+	// stable in the volume server name (not position!)
+	// and if more are added the correct portion will move (yay md5!)
+	var sortvols []sortvol
+	for _, v := range volumes {
+		hash := md5.New()
+		hash.Write(key)
+		hash.Write([]byte(v))
+		score := hash.Sum(nil)
+		sortvols = append(sortvols, sortvol{score, v})
 	}
-	return tokenizedSequence
+	sort.Stable(byScore(sortvols))
+	// go should have a map function
+	// this adds the subvolumes
+	var ret []string
+	for i := 0; i < count; i++ {
+		sv := sortvols[i]
+		var volume string
+		if svcount == 1 {
+			// if it's one, don't use the path structure for it
+			volume = sv.volume
+		} else {
+			// use the least significant compare dword for the subvolume
+			// using only a byte would cause potential imbalance
+			svhash := uint(sv.score[12])<<24 + uint(sv.score[13])<<16 +
+				uint(sv.score[14])<<8 + uint(sv.score[15])
+			volume = fmt.Sprintf("%s/sv%02X", sv.volume, svhash%uint(svcount))
+		}
+		ret = append(ret, volume)
+	}
+	//fmt.Println(string(key), ret[0])
+	return ret
 }
